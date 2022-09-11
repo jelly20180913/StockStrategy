70a4f51ea1622a9419a1e551f9b277ee718b96be
jelly20180913 <43227923+jelly20180913@users.noreply.github.com>
2022-09-11 10:18:22 +0800

    上傳StockStrategy 的service

---
+++ WebApiService/Services/Implement/StockStrategyService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiService.Models;
+using WebApiService.Services.Implement.Tables;
+using WebApiService.Services.Interface;
+using WebApiService.Services.Interface.Tables;
+
+namespace WebApiService.Services.Implement
+{
+	public class StockStrategyService: IStockStrategyService
+	{
+		private IStockService _stockService; 
+		/// <summary>
+		/// Dependence Injection
+		/// </summary>
+
+		public StockStrategyService(IStockService stockService)
+		{
+			this._stockService = stockService; 
+		}
+		/// <summary>
+		/// get vote item list
+		/// </summary>
+		/// <param name="className"></param>
+		/// <returns></returns>
+		public List<DataModel.Stock.Stock> GetStockList(string parameter, string mode)
+		{
+			List<DataModel.Stock.Stock> _StockList = new List<DataModel.Stock.Stock>();
+			if (mode == "Code") _StockList = this._stockService.GetByCode(parameter).ToList();
+			else if(mode == "Date") _StockList = this._stockService.GetByDate(parameter).ToList();
+			else if (mode == "Predays") _StockList = this._stockService.GetByPredays(parameter).ToList(); //取前N日會有六日問題
+			return _StockList;
+		}
+		public bool InsertStock(List<Stock> s)
+		{
+			this._stockService.MiltiCreate(s);
+			return true;
+		}
+		public bool UpdateStock(Stock s)
+		{
+			this._stockService.Update(s);
+			return true;
+		}
+	}
+}
---
+++ WebApiService/Services/Interface/IStockStrategyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiService.Models;
+
+namespace WebApiService.Services.Interface
+{
+	public interface IStockStrategyService
+	{
+		List<DataModel.Stock.Stock> GetStockList(string parameter, string mode);
+		bool InsertStock(List<Stock> s);
+		bool UpdateStock(Stock s);
+	}
+}
